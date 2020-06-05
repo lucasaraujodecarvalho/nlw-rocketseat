bc6e7abdc369598a46495f4a9ccbba5b8e1ff073
lucasaraujodecarvalho <lucas.car.araujo@gmail.com>
2020-06-04 23:29:59 -0300

    Aula 03

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -33,7 +33,7 @@ function getCities(event) {
     .then( cities => {
         
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value = "${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value = "${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -44,3 +44,38 @@ function getCities(event) {
 document
    .querySelector("select[name=uf]")
    .addEventListener("change", getCities )
+
+
+
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+
+    itemLi.classList.toggle("selected")
+
+    const itemId = itemLi.dataset.id
+
+    const alreadySelected = selectedItems.findIndex( item => item == itemId)
+
+    if (alreadySelected >= 0) {
+        const filteredItems = selectedItems.filter( item => {
+            const itemIsDifferent = item != itemId
+            return itemIsDifferent
+        } )
+        selectedItems = filteredItems
+    } else {
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems
+    
+}
